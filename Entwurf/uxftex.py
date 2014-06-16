79826bc758ed50383ab40e7fbd21582779c3f68d
Matthias Klatte <p.roni@proni.net>
2014-06-16 18:16:49 +0200

    Aufbereitung um Methoden and stuff ergänzt, reactor.tex formatiert, uxftex hart überarbeitet

--- Entwurf/uxftex.py
+++ Entwurf/uxftex.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python
+# -*- coding: utf8 -*-
 
 import sys
 import xml.etree.ElementTree as ET
+import re
 
 inputfile = ""
 outputfile = ""
-d = True
+d = False
 
 def writeToFile(data):
 	with open(outputfile, "a") as myfile:
-		myfile.write("\subsection{" + data["classname"] + "}\n")
+		myfile.write("\n\n\subsection{" + data["classname"] + "}\n")
 		myfile.write(data["description"] + "\n")
 		myfile.write("\n")
 		if "attributes" in data.keys() and len(data["attributes"]) > 0:
 			myfile.write("\subsubsection{Attributes}\n")
 			myfile.write("\\begin{itemize}\n")
 			for a in data["attributes"]:
-				myfile.write("\\item " + a["visibility"] + " " + a["type"] + " " + a["name"] + "\nTODO: Description\n")
+				if "visibility" in a:
+					myfile.write("\t\\item " + a["visibility"] + " " + a["type"] + " " + a["name"] + "\\\\\n\tTODO: Description\n")
 			myfile.write("\\end{itemize}\n")
 		if "methods" in data.keys() and len(data["methods"]) > 0:
 			myfile.write("\subsubsection{Methods}\n")
 			myfile.write("\\begin{itemize}\n")
 			for a in data["methods"]:
-				myfile.write("\\item " + a["visibility"] + " " + a["type"] + " " + a["name"] + "\nTODO: Description\n")
+				if "visibility" in a:
+					myfile.write("\t\\item " + a["visibility"] + " " + a["type"] + " " + a["name"] + "\\\\\n\tTODO: Description\n")
 			myfile.write("\\end{itemize}\n")
 
 def visibility(symbol):
@@ -46,22 +50,32 @@ def parsePnl(pnlattr):
 	else:
 		state = 0
 		# classname irgendwie machen
+		result["classname"] = ""
+		result["description"] = "TODO: Description"
 		result["attributes"] = []
 		result["methods"] = []
 		k = ["classname", "attributes", "methods"]
 		while cursor < len(pnlattr):
 			line = pnlattr[cursor]
+			line = re.sub(r'/(.*)/', '\\\\textit{\\1}', line)
+			line = line.replace('_', '\_')
 			if line == "--":
 				state += 1
-			if state in range(1, 2):
+			if state == 0:
+				# if line[0].encode('utf8') == u"\u00AB".encode('utf8'):
+					# result["classname"]+= line[1:-1].capitalize() + " "
+				result["classname"]+= line + " "
+			if state == 1 or state == 2:
 				o = {}
-				if line[1] == " ":
-					o["visibility"] = visibility(line[0])
-					line = line[2:]
-				x = line.split(":")
-				o["type"] = x[-1]
-				o["name"] = ":".join(x[0, -1]).strip()
-				result[k[state]].add(o)
+				if len(line) > 1:
+					print(result["classname"] + " -> " + k[state])
+					if line[1] == " ":
+						o["visibility"] = visibility(line[0])
+						line = line[2:]
+					x = line.split(":")
+					o["type"] = x[-1]
+					o["name"] = ":".join(x[0:-1]).strip()
+					result[k[state]].append(o)
 			cursor += 1
 	return result
 
